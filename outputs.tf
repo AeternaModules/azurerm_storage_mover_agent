9c2d0f9689814cbaa34f0fa978624b583e67e76a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "storage_mover_agents" {
-  description = "All storage_mover_agent resources"
-  value       = azurerm_storage_mover_agent.storage_mover_agents
-}
 output "storage_mover_agents_arc_virtual_machine_id" {
-  description = "List of arc_virtual_machine_id values across all storage_mover_agents"
-  value       = [for k, v in azurerm_storage_mover_agent.storage_mover_agents : v.arc_virtual_machine_id]
+  description = "Map of arc_virtual_machine_id values across all storage_mover_agents, keyed the same as var.storage_mover_agents"
+  value       = { for k, v in azurerm_storage_mover_agent.storage_mover_agents : k => v.arc_virtual_machine_id }
 }
 output "storage_mover_agents_arc_virtual_machine_uuid" {
-  description = "List of arc_virtual_machine_uuid values across all storage_mover_agents"
-  value       = [for k, v in azurerm_storage_mover_agent.storage_mover_agents : v.arc_virtual_machine_uuid]
+  description = "Map of arc_virtual_machine_uuid values across all storage_mover_agents, keyed the same as var.storage_mover_agents"
+  value       = { for k, v in azurerm_storage_mover_agent.storage_mover_agents : k => v.arc_virtual_machine_uuid }
 }
 output "storage_mover_agents_description" {
-  description = "List of description values across all storage_mover_agents"
-  value       = [for k, v in azurerm_storage_mover_agent.storage_mover_agents : v.description]
+  description = "Map of description values across all storage_mover_agents, keyed the same as var.storage_mover_agents"
+  value       = { for k, v in azurerm_storage_mover_agent.storage_mover_agents : k => v.description }
 }
 output "storage_mover_agents_name" {
-  description = "List of name values across all storage_mover_agents"
-  value       = [for k, v in azurerm_storage_mover_agent.storage_mover_agents : v.name]
+  description = "Map of name values across all storage_mover_agents, keyed the same as var.storage_mover_agents"
+  value       = { for k, v in azurerm_storage_mover_agent.storage_mover_agents : k => v.name }
 }
 output "storage_mover_agents_storage_mover_id" {
-  description = "List of storage_mover_id values across all storage_mover_agents"
-  value       = [for k, v in azurerm_storage_mover_agent.storage_mover_agents : v.storage_mover_id]
+  description = "Map of storage_mover_id values across all storage_mover_agents, keyed the same as var.storage_mover_agents"
+  value       = { for k, v in azurerm_storage_mover_agent.storage_mover_agents : k => v.storage_mover_id }
 }
 
